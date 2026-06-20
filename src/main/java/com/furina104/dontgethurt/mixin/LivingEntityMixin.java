d7be42b8e194a260946888f7d233034861cca220
Furina104 <furina104@users.noreply.github.com>
2026-06-20 17:17:47 +0800

    fix: 添加 ServerWorld import

--- src/main/java/com/furina104/dontgethurt/mixin/LivingEntityMixin.java
+++ src/main/java/com/furina104/dontgethurt/mixin/LivingEntityMixin.java
@@ -4,6 +4,7 @@ import com.furina104.dontgethurt.DontGetHurt;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
